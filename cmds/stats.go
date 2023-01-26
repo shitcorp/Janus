f641b3a44608d8868252adda03eb5de40810adc1
Husky <39809509+Huskydog9988@users.noreply.github.com>
2023-01-26 16:48:46 -0500

    migrate cmds to ken

--- cmds/stats.go
+++ cmds/stats.go
@@ -2,22 +2,65 @@ package cmds
 
 import (
   "context"
+  "github.com/bwmarrin/discordgo"
   "github.com/diamondburned/arikawa/v3/api"
   "github.com/diamondburned/arikawa/v3/api/cmdroute"
   "github.com/diamondburned/arikawa/v3/utils/json/option"
   log "github.com/sirupsen/logrus"
+  "github.com/zekrotja/ken"
 )
 
-func (h *Handler) cmdStats(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+//func (h *Handler) cmdStats(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+//
+//	_, stats, err := h.api.Website.Stats()
+//	if err != nil {
+//		log.Error(err)
+//	}
+//
+//	return &api.InteractionResponseData{
+//		Content:         option.NewNullableString(stats.Data.CurrentLive),
+//		AllowedMentions: &api.AllowedMentions{}, // don't mention anyone
+//	}
+//}
 
+type StatsCommand struct{}
 
-  _, stats, err := h.api.Website.Stats()
-  if err != nil {
-    log.Error(err)
-  }
+var (
+  _ ken.SlashCommand = (*StatsCommand)(nil)
+  _ ken.DmCapable    = (*StatsCommand)(nil)
+)
 
-  return &api.InteractionResponseData{
-    Content:         option.NewNullableString(stats.Data.CurrentLive),
-    AllowedMentions: &api.AllowedMentions{}, // don't mention anyone
-  }
+func (c *StatsCommand) Name() string {
+  return "stats"
+}
+
+func (c *StatsCommand) Description() string {
+  return "Basic ping command"
+}
+
+func (c *StatsCommand) Version() string {
+  return "1.0.0"
+}
+
+func (c *StatsCommand) Type() discordgo.ApplicationCommandType {
+  return discordgo.ChatApplicationCommand
+}
+
+func (c *StatsCommand) Options() []*discordgo.ApplicationCommandOption {
+  return []*discordgo.ApplicationCommandOption{}
+}
+
+func (c *StatsCommand) IsDmCapable() bool {
+  return true
+}
+
+func (c *StatsCommand) Run(ctx ken.Context) (err error) {
+
+  err = ctx.Respond(&discordgo.InteractionResponse{
+    Type: discordgo.InteractionResponseChannelMessageWithSource,
+    Data: &discordgo.InteractionResponseData{
+      Content: "pong",
+    },
+  })
+  return
 }
--- cmds/utils.go
+++ cmds/utils.go
@@ -2,83 +2,9 @@ package cmds
 
 import (
 	"github.com/diamondburned/arikawa/v3/api"
-	"github.com/diamondburned/arikawa/v3/api/cmdroute"
 	"github.com/diamondburned/arikawa/v3/discord"
-	"github.com/diamondburned/arikawa/v3/state"
 	"github.com/diamondburned/arikawa/v3/utils/json/option"
-	"github.com/shitcorp/janus/scapi"
-	"github.com/spf13/viper"
-	"log"
 )
-
-var commands = []api.CreateCommandData{
-	{
-		Name:        "ping",
-		Description: "ping pong!",
-	},
-	{
-		Name:        "echo",
-		Description: "echo back the argument",
-		Options: []discord.CommandOption{
-			&discord.StringOption{
-				OptionName:  "argument",
-				Description: "what's echoed back",
-				Required:    true,
-			},
-		},
-	},
-	{
-		Name:        "thonk",
-		Description: "biiiig thonk",
-	},
-	{
-		Name:        "stats",
-		Description: "Gets star citizen stats",
-	},
-}
-
-func OverwriteCommands(s *state.State) error {
-
-	if viper.GetString("APP_ENV") == "production" {
-		return cmdroute.OverwriteCommands(s, commands)
-	} else {
-		app, err := s.CurrentApplication()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		snowflake, err := discord.ParseSnowflake(viper.GetString("TEST_GUILD_ID"))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		guildID := discord.GuildID(snowflake)
-
-		_, err = s.BulkOverwriteGuildCommands(app.ID, guildID, commands)
-		return err
-	}
-}
-
-type Handler struct {
-	*cmdroute.Router
-	State *state.State
-	api   *scapi.Api
-}
-
-func NewHandler(s *state.State) *Handler {
-	h := &Handler{State: s}
-
-	h.api = scapi.CreateApiClient(viper.GetString("SCAPI_TOKEN"), "Janus Bot")
-
-	h.Router = cmdroute.NewRouter()
-	// Automatically defer handles if they're slow.
-	h.Use(cmdroute.Deferrable(s, cmdroute.DeferOpts{}))
-	h.AddFunc("echo", h.cmdEcho)
-	h.AddFunc("thonk", h.cmdThonk)
-	h.AddFunc("stats", h.cmdStats)
-
-	return h
-}
 
 func errorResponse(err error) *api.InteractionResponseData {
 	return &api.InteractionResponseData{
